73f81c8e9969238a67f8b9078a6c18a52aa13f66
Rt <philmol@yandex.ru>
2019-12-24 19:51:48 +0300

    still dont working(

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Container>
+      <ChatWindow @send="axios.post('http://188.225.47.187/api/chat/getmessages.php', JSON.stringify($event)).then((response) => {console.log(response);}).catch((error) => {console.log(error);});">
+        <div v-for="message in messages" :key="message">
+          <ChatMessage :username="message.author" :datetime="message.datetime">{{message.text}}</ChatMessage>
+        </div>
+      </ChatWindow>
+    </Container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
+import Container from './components/Container.vue'
 
 export default {
   name: 'app',
+  data() {
+  return {messages : []};
+  },
+  mounted(){
+  this.getmessages();
+  },
+  methods:{
+    getmessages(){
+    setInterval(() => axios.get('http://188.225.47.187/api/chat/getmessages.php').then(response => {this.messages = response.data.reverse()}), 2000);
+    }
+  },
   components: {
-    HelloWorld
+    ChatMessage, ChatWindow, Container
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+body {
+  margin: 0;
+  background-color: #f9f9fa;
+}
+
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <slot> </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+</style>
